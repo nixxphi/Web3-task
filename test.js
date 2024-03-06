6ae52e31cdc3babad2803fc58b921637bc173d62
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-06 07:09:24 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,36 @@
 
 const User = require('./app.js');
 
-// Create a new User object
+// Lets give Tappi a life 
 const user = new User('Tappi');
 
-// Add data to the "Fave movies" category
+// Adding false personal information
+user.addPersonalInformation({
+    age: 37,
+    profession: 'Product designer',
+    health: {
+        medicalHistory: ['Seasonal malaria'],
+        habits: ['Friendly mockery']
+    }
+});
+
+// Including fake education and certification data
+user.addMoreData('Education and certification data', 'Bachelor of Science in Software Engineering - MIT');
+user.addMoreData('Education and certification data', 'PhD in Engineering - Princeton University');
+user.addMoreData('Education and certification data', 'Certified Professional Dreams');
+
+console.log("User's personal information:", user.getPersonalInformation());
+console.log("User's education and certification data:", user.getEducationCertificationData());
+
+//testing random data section 
 user.addMoreData('FaveMovies', 'Tenet');
 user.addMoreData('FaveMovies', 'The Terminator 2');
 user.addMoreData('FaveMovies', 'Avatar');
-
-// Add data to the "Favourite music artists" category
 user.addMoreData('FavouriteMusicArtists', 'RedCyX');
-user.addMoreData('FavouriteMusicArtists', 'Jay Z');
+user.addMoreData('FavouriteMusicArtists', 'Abba');
 user.addMoreData('FavouriteMusicArtists', 'BNXN');
-user.addMoreData('FavouriteMusicArtists', 'David');
+user.addMoreData('FavouriteMusicArtists', 'Davido');
 
-
-// Call the dynamically generated get method for each category
-console.log(user.getFaveMoviesData()); // Output: ['Tenet', 'The Terminator 2', 'Avatar']
-console.log(user.getFavouriteMusicArtistsData()); // Output: ['RedCyX', 'Jay Z', 'BNXN', 'David']
+console.log(user.getFaveMoviesData());
+console.log(user.getFavouriteMusicArtistsData()); 
 ￼Enter
