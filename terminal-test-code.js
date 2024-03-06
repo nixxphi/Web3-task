505b466bdd247a7cdf952bdc7edf632da3557834
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-03-06 21:27:34 +0100

    Update terminal-test-code.js
    I don't wanna talk about it

--- terminal-test-code.js
+++ terminal-test-code.js
@@ -106,7 +106,7 @@ class User {
 let users = [];
 
 function createSampleUser() {
-    rl.question('Enter the name of the user: ', (name) => {
+    rl.question('Your name?: ', (name) => {
         const newUser = new User(name);
         users.push(newUser);
         console.log(`User "${name}" created successfully.`);
@@ -115,7 +115,7 @@ function createSampleUser() {
 }
 
 function addData(user) {
-    rl.question('Enter the category of data you want to add (e.g., personal information, banking data, education data, more data): ', (category) => {
+    rl.question('What category of data do you want to add today (e.g., personal information, banking data, education data, more data): ', (category) => {
         switch (category.toLowerCase()) {
             case 'personal information':
                 rl.question('Enter the personal information: ', (info) => {
@@ -140,7 +140,7 @@ function addData(user) {
                 break;
             case 'more data':
                 rl.question('Enter the category of more data: ', (moreCategory) => {
-                    rl.question('Enter the data: ', (data) => {
+                    rl.question('Name your category: ', (data) => {
                         user.addMoreData(moreCategory, data);
                         saveUserData();
                         rl.close();
@@ -159,24 +159,24 @@ function saveUserData() {
     const jsonData = JSON.stringify(users, null, 2);
     fs.writeFile('users.json', jsonData, (err) => {
         if (err) throw err;
-        console.log('User data saved to users.json file.');
+        console.log('Sample user data saved to users.json file.');
     });
 }
 
 function startTerminalInterface() {
-    console.log('Welcome to the Sample User Creation Terminal Interface!');
+    console.log('Welcome to our Sample User Creation Terminal Interface! Front-end was takign forever...');
     rl.question('Would you like to create a sample user? (yes/no): ', (answer) => {
         if (answer.toLowerCase() === 'yes') {
             createSampleUser();
         } else {
-            console.log('Exiting...');
+            console.log('Bye...');
             rl.close();
         }
     });
 }
 
 startTerminalInterface();
-￼Enter        }
+        }
         this.moreData[category][subtopic].push(data);
     }
 }
